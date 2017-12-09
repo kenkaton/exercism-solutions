dce8b39b3ae3d1cec5c6a688eb14ad147e756837
kenkaton <putsken@gmail.com>
2017-12-09 14:02:23 +0900

    Change method from each_with_object to select

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,7 +2,7 @@ class Raindrops
   RAIN_SOUNDS = {3 => "Pling", 5 => "Plang", 7 => "Plong"}
 
   def self.convert(num)
-    raindrop_speak = RAIN_SOUNDS.each_with_object("") {|(key_num, rain_sound), sound| sound << rain_sound if num % key_num == 0}
+    raindrop_speak = RAIN_SOUNDS.select{|key_num, v| num % key_num == 0}.values.join
     raindrop_speak.empty? ? num.to_s : raindrop_speak
   end
 end
