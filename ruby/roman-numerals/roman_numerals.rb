68fc0f6c4d2751d287ce5840b6768cf8bcec96ea
kenkaton <putsken@gmail.com>
2017-12-31 15:39:35 +0900

    Use hash and map

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,14 +1,16 @@
 class Integer
   def to_roman
-    num = self
-    units = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    letters = %w(M CM D CD C XC L XL X IX V IV I)
-    units.each_with_index.with_object("") {|(unit, i), romanized|
-      num.div(unit).times {
-        romanized << letters[i]
-      }
-      num %= unit
+    roman = {
+      M:  1000,
+      CM: 900, D: 500, CD: 400, C: 100,
+      XC: 90,  L: 50,  XL: 40,  X: 10,
+      IX: 9,   V: 5,   IV: 4,   I: 1,
     }
+    num = self
+    roman.map do |letter, val|
+      q, num = num.divmod(val)
+      letter.to_s * q
+    end.join
   end
 end
 
